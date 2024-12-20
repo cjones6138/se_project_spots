7ad44f74fd3e0267cbb14c58ae2797ee3a83ccb7
Charles Jones <charles.e.jones28@gmail.com>
2024-12-20 08:50:31 -0500

    Initial js and pictures

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Dark Eclipse",
+    link: "https://unsplash.com/photos/digital-wallpaper-of-eclipse-_ok8uVzL2gI",
+  },
+  {
+    name: "Red Sky",
+    link: "https://unsplash.com/photos/shooting-star-in-night-sky-5LOhydOtTKU",
+  },
+  {
+    name: "Green Aura",
+    link: "https://unsplash.com/photos/northern-lights-during-night-time-R3pUGn5YiTg",
+  },
+  {
+    name: "Harvest Moon",
+    link: "https://unsplash.com/photos/brown-planet-QQ9MzSs-o1I",
+  },
+  {
+    name: "Impact Crater",
+    link: "https://unsplash.com/photos/a-large-crater-in-the-middle-of-a-desert-E7q00J_8N7A",
+  },
+  {
+    name: "Shuttle Launch",
+    link: "https://unsplash.com/photos/ray-of-light-near-body-of-water--p-KCm6xB9I",
+  },
+];
