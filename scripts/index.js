7c81ccf468a9554096b7deb6dacad6055beea1f2
Charles Jones <charles.e.jones28@gmail.com>
2024-12-31 11:58:56 -0500

    Complete rendering cards from array of objects.

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
     name: "Dark Eclipse",
-    link: "https://unsplash.com/photos/digital-wallpaper-of-eclipse-_ok8uVzL2gI",
+    link: "https://images.unsplash.com/photo-1504192010706-dd7f569ee2be?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Red Sky",
-    link: "https://unsplash.com/photos/shooting-star-in-night-sky-5LOhydOtTKU",
+    link: "https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Green Aura",
-    link: "https://unsplash.com/photos/northern-lights-during-night-time-R3pUGn5YiTg",
+    link: "https://images.unsplash.com/photo-1443926818681-717d074a57af?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Harvest Moon",
-    link: "https://unsplash.com/photos/brown-planet-QQ9MzSs-o1I",
+    link: "https://images.unsplash.com/photo-1443456066412-3e3ea69ee37c?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Impact Crater",
-    link: "https://unsplash.com/photos/a-large-crater-in-the-middle-of-a-desert-E7q00J_8N7A",
+    link: "https://images.unsplash.com/photo-1451188502541-13943edb6acb?q=80&w=1228&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Shuttle Launch",
-    link: "https://unsplash.com/photos/ray-of-light-near-body-of-water--p-KCm6xB9I",
+    link: "https://images.unsplash.com/photo-1457364887197-9150188c107b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
@@ -31,15 +31,31 @@ const profileDescriptionElement = document.querySelector(
 );
 const profileEditButton = document.querySelector(".profile__edit-button");
 
-const modalCloseButton = document.querySelector(".modal__close-button");
-const modalSubmitButton = document.querySelector(".modal__submit-button");
-
 const editModal = document.querySelector("#edit-profile-modal");
+const editModalCloseButton = document.querySelector(".modal__close-button");
 const editModalFormElement = editModal.querySelector(".modal__form");
 const editModalInputName = editModal.querySelector("#profile-name-input");
 const editModalInputDescription = editModal.querySelector(
   "#profile-description-input"
 );
+
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  const cardSourceEl = cardElement.querySelector(".card__image");
+
+  cardTitleEl.textContent = data.name;
+  cardSourceEl.setAttribute("src", data.link);
+  cardSourceEl.setAttribute("alt", data.name);
+
+  return cardElement;
+}
 
 function openModal() {
   editModal.classList.remove("modal__visibility");
@@ -60,9 +76,13 @@ function handleEditFormSubmit(evt) {
   closeModal();
 }
 
-// Connect the handler to the form, so it will watch for the submit event.
 editModalFormElement.addEventListener("submit", handleEditFormSubmit);
 
 profileEditButton.addEventListener("click", openModal);
 
-modalCloseButton.addEventListener("click", closeModal);
+editModalCloseButton.addEventListener("click", closeModal);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
